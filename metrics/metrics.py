65ff5e0899f7f1a0003f3e4399dddeedf1340bbb
UmutAA <201361309+UmutAA@users.noreply.github.com>
2026-09-02 14:03:10 +0300

    update sigmoid and calculate_bce function for a cleaner structure

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -24,28 +24,26 @@ def calculate_mse(y_trues: np.ndarray, y_predicteds: np.ndarray) -> float:
 def calculate_rmse(y_trues: np.ndarray, y_predicteds: np.ndarray) -> float:
     return math.sqrt(calculate_mse(y_trues, y_predicteds))
 
-def sigmoid(x: np.ndarray) -> float:
-    z = 1 / (1 + np.exp(-x))
-    return z
+def sigmoid(z: np.ndarray) -> np.ndarray:
+    p = 1 / (1 + np.exp(-z))
+    eps = 1e-15
+    p = np.clip(p, eps, 1 - eps)
+    return p
 
-def calculate_bce(x: np.ndarray, y: np.ndarray) -> float:
+def calculate_bce(y: np.ndarray, p: np.ndarray) -> float:
     """
     Calculates and returns binary cross entropy error (Log loss)
     """
-    if not isinstance(x, np.ndarray):
-        x = np.asarray(x, dtype=np.float64)
-        
     if not isinstance(y, np.ndarray):
         y = np.asarray(y, dtype=np.float64)
+        
+    if not isinstance(p, np.ndarray):
+        p = np.asarray(p, dtype=np.float64)
 
-    n = y.shape[0]
+    n = p.shape[0]
 
     if n == 0:
         raise ZeroDivisionError("Dimension can't be 0")
-
-    p = sigmoid(x)
-    eps = 1e-15
-    p = np.clip(p, eps, 1 - eps)
     
     bce_matrix = y * np.log(p) + (1 - y) * np.log(1 - p)
     bce = - np.sum(bce_matrix) / float(n)
